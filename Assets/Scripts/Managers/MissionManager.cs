21a3aa9b7515a7540819d5bb783746010d27483f
Bibni <h4ck3r59@hotmail.fr>
2018-04-22 17:26:43 +0200

    Permit to restart at the end.

--- Assets/Scripts/Managers/MissionManager.cs
+++ Assets/Scripts/Managers/MissionManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MissionManager : MonoBehaviour {
 
@@ -20,6 +21,22 @@ public class MissionManager : MonoBehaviour {
 
         player.OnDetected += PlayerDetected;
         player.OnCatched += PlayerLose;
+    }
+
+    private void Update()
+    {
+        if(IsGameOver)
+        {
+            if(Input.GetButtonDown("Submit"))
+            {
+                Restart();
+            }
+        }
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void PlayerDetected()
